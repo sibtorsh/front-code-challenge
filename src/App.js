11a9ce5bbeea60477bfb7e1df25049738b0c2b95
Elnaz <elnazmalakouti@gmail.com>
2023-07-03 13:21:40 +0330

    create router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import Users from "./Pages/dashboard/users";
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
 function App() {
+
+    const router = createBrowserRouter([
+        {
+            path : '/',
+            children: [
+                {
+                    path : '/dashboard',
+                    children : [
+                        {
+                            path : '/dashboard/users',
+                            element : <Users/> 
+                        }
+                    ]
+                }
+            ]
+        }
+    ])
+
     return (
         <div className={'min-h-screen p-2 w-full h-full flex justify-center items-center'}>
-            <Users/>
+            <RouterProvider router={router} />
         </div>
     );
 }
